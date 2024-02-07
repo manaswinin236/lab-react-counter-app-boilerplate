a33e26e5f54ac920ccb7e039d3dfe8d8dbb337be
manaswini <manaswini0623@gmail.com>
2024-02-07 10:00:10 +0530

    update

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import React from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const add = () => {
+    setCount(count + 1);
+  };
+
+  const subtract = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h2>Counter App</h2>
+        <div id="counter">{count}</div>
+        <div className="buttons">
+          <button id="add" onClick={add}>
+            +
+          </button>
+          <button id="subtract" onClick={subtract}>
+            -
+          </button>
+          <button id="reset" onClick={reset}>
+            reset
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
